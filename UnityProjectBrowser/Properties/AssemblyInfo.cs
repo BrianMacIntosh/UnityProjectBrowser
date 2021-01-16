0534a5aef262b50e5e37d8c78b38148b328cccb4
Brian MacIntosh <brian@brianmacintosh.com>
2021-01-16 10:18:11 -0800

    release version

--- UnityProjectBrowser/Properties/AssemblyInfo.cs
+++ UnityProjectBrowser/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brian MacIntosh")]
 [assembly: AssemblyProduct("Unity Project Browser")]
-[assembly: AssemblyCopyright("Copyright © Brian MacIntosh 2018")]
+[assembly: AssemblyCopyright("Copyright © Brian MacIntosh 2018-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
